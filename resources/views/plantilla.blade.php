d35974ff686a0bce0503f1ce5ce4fa0e12e86f10
Rafa <rafaelmontavatorregrosa@gmail.com>
2021-02-09 00:14:43 +0000

    Estilos principales del header

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -3,7 +3,9 @@
 	<head>
 		<title> @yield('titulo') </title>
 		<link rel="stylesheet" type="text/css" href="/css/app.css">
+        <link rel="stylesheet" type="text/css" href="/css/scss/header-style.css">
 		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/scss/header-style.css') }}" rel="stylesheet">
 	</head>
 	<body>
 		@include('partials.nav')
